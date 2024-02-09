2a755c617f199cffe904d1d61ef23341807a124c
diego delgado <diegodelgado045@hotmail.com>
2024-02-09 10:24:52 -0500

    The file structure was defined and the router was created

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+	return (
+		<title>Credit Card Payment</title>
+	)
+}
---
+++ src/components/tools/styleContent.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const FooterApp = styled.footer `
+	background-color: #333;
+	color: #A1A5B7 !important; 
+	font-weight: 500 !important;
+	font-size: 0.8rem;
+	padding: 5px; 
+	text-align: center; 
+	z-index: 10;
+`
